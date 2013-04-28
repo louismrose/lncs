cbfe26dbb51304efc4d08d3c28eea3d22e8ba63f
Louis Rose <louis.rose@york.ac.uk>
2013-04-28 22:19:01 +0200

    More complete initialiser.

--- lib/lncs/cli.rb
+++ lib/lncs/cli.rb
@@ -5,7 +5,7 @@ module LNCS
   class CLI < Thor
     desc "init", "Start an lncs project in the current directory"
     def init
-      Initialiser.new(File.expand_path('.')).run
+      Initialiser.new.run
     end
     
     desc "clean", "Remove all working directories"
--- lib/lncs/initialiser.rb
+++ lib/lncs/initialiser.rb
@@ -1,18 +1,21 @@
 module LNCS
-  class Initialiser
+  class Initialiser < Thor
     include Thor::Actions
     
-    def initialize(dir)
-      @dir = dir
+    def self.source_root
+      File.join(File.dirname(__FILE__), '..', '..', 'templates')
     end
     
-    def run
-      create_manifest
-    end
-  
-  private
-    def create_manifest
-      copy_file '../../templates/manifest.json', File.join(@dir, 'manifest.json')
+    no_commands do
+      def run
+        copy_file 'manifest.json'
+        copy_file 'main.tex'
+        copy_file 'llncs.cls'
+        copy_file 'sprmindx.sty'
+        copy_file 'front_matter/organisation.tex'
+        copy_file 'front_matter/preface.tex'
+        copy_file 'front_matter/sponsors.tex'
+      end
     end
   end
 end
